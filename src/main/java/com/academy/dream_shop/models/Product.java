046daeb9999c55679e2b9a0a3e6d205b07b39543
svetoslavss <svetoslavd.stoyanovs@gmail.com>
2025-01-13 22:56:07 +0200

    Annotation added from lombok mark as Entity

--- src/main/java/com/academy/dream_shop/models/Product.java
+++ src/main/java/com/academy/dream_shop/models/Product.java
@@ -1,4 +1,13 @@
 package com.academy.dream_shop.models;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
 }
