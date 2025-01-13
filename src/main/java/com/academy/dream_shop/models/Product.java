4ab3d31622c1c558d5f1fd3cba088c30aefcdee5
svetoslavss <svetoslavd.stoyanovs@gmail.com>
2025-01-13 23:25:05 +0200

    Product entity added annotations

--- src/main/java/com/academy/dream_shop/models/Product.java
+++ src/main/java/com/academy/dream_shop/models/Product.java
@@ -1,2 +1,32 @@
-package com.academy.dream_shop.models;public class Product {
+package com.academy.dream_shop.models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private int quantity;
+    private BigDecimal price;
+    private String description;
+
+    private Category category;
+    private List<Image> images;
 }
