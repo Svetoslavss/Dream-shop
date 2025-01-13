7ef40391367645ed1e7f7efb816269b44c2c7c87
svetoslavss <svetoslavd.stoyanovs@gmail.com>
2025-01-13 23:00:29 +0200

    Product model annotations added

--- src/main/java/com/academy/dream_shop/models/Product.java
+++ src/main/java/com/academy/dream_shop/models/Product.java
@@ -1,2 +1,26 @@
-package com.academy.dream_shop.models;public class Product {
+package com.academy.dream_shop.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String description;
+    private int quantity;
+    private BigDecimal price;
 }
