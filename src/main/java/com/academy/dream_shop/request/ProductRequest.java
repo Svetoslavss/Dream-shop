b8bd06c47c25a349e6c722a9a45d9b27d05a08aa
Svetoslavss <svetoslavd.stoyanov@gmail.com>
2025-01-20 22:25:08 +0200

    New Product request package and class added

---
+++ src/main/java/com/academy/dream_shop/request/ProductRequest.java
@@ -0,0 +1,22 @@
+package com.academy.dream_shop.request;
+
+import com.academy.dream_shop.models.Category;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductRequest {
+
+    private Long id;
+    private String name;
+    private String brand;
+    private BigDecimal price;
+    private int inventory;
+    private String description;
+    private Category category;
+
+}
