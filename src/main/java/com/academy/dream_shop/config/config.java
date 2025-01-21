f0f12a083349575cccfd6c1d834f3d32b3595b84
Svetoslavss <svetoslavd.stoyanov@gmail.com>
2025-01-22 00:12:08 +0200

    Configure Spring Security: Added role-based access control and custom login handling

---
+++ src/main/java/com/academy/dream_shop/config/config.java
@@ -0,0 +1,37 @@
+package com.academy.dream_shop.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class config {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/admin").hasRole("ADMIN")
+                        .requestMatchers("/auth/user").hasRole("USER")
+                        .anyRequest().authenticated())
+                .formLogin(login ->
+                        login.loginPage("/login")
+                                .defaultSuccessUrl("/login", true)
+                                .loginProcessingUrl("/authenticate")
+                                .failureUrl("/login?failure=true"));
+
+
+        return http.build();
+    }
+
+}
