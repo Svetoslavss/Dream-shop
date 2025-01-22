10064e792ece0af673ee679cb2a28ae8a641ae0f
Svetoslavss <svetoslavd.stoyanov@gmail.com>
2025-01-22 22:16:14 +0200

    Created ICategoryService for implementing the bussines logic

--- src/main/java/com/academy/dream_shop/services/product/category/ICategoryService.java
+++ src/main/java/com/academy/dream_shop/services/product/category/ICategoryService.java
@@ -1,4 +1,16 @@
 package com.academy.dream_shop.services.product.category;
 
+import com.academy.dream_shop.models.Category;
+import com.academy.dream_shop.models.Product;
+import com.academy.dream_shop.request.CategoryUpdateRequest;
+import com.academy.dream_shop.request.ProductRequest;
+
 public interface ICategoryService {
+
+    Category getCategoryById(Long id);
+    Category addProductInCategory(Category category, ProductRequest req);
+    void deleteCategoryId(Long id);
+    Category updateCategory(CategoryUpdateRequest req , Long categoryId);
+
+
 }
