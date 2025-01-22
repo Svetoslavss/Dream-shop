fe950e3203e08283b050f70adf0d0553afebf2eb
Svetoslavss <svetoslavd.stoyanov@gmail.com>
2025-01-22 22:40:56 +0200

    Add CategoryNotFound exception for handling category retrieval errors

---
+++ src/main/java/com/academy/dream_shop/exceptions/CategoryNotFound.java
@@ -0,0 +1,8 @@
+package com.academy.dream_shop.exceptions;
+
+public class CategoryNotFound extends RuntimeException {
+
+    public CategoryNotFound(String message) {
+        super(message);
+    }
+}
