a9b1790297e1d639d78e4b9727292cd707543ffb
Svetoslavss <svetoslavd.stoyanov@gmail.com>
2025-01-22 21:27:34 +0200

    Add new business logic for updating and adding a product

--- src/main/java/com/academy/dream_shop/services/product/ProductService.java
+++ src/main/java/com/academy/dream_shop/services/product/ProductService.java
@@ -1,13 +1,19 @@
 package com.academy.dream_shop.services.product;
 
 import com.academy.dream_shop.exceptions.ProductNotFoundException;
+import com.academy.dream_shop.models.Category;
 import com.academy.dream_shop.models.Product;
+import com.academy.dream_shop.repository.CategoryRepository;
 import com.academy.dream_shop.repository.ProductRepository;
+import com.academy.dream_shop.request.ProductRequest;
+import com.academy.dream_shop.request.ProductUpdateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
 
 @Service
 public class ProductService implements IProductService {
@@ -15,11 +21,32 @@ public class ProductService implements IProductService {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Override
-    public Product addProduct(Product product) {
-        return productRepository.save(product);
+    public Product addProduct(ProductRequest req) {
+        Category category = Optional.ofNullable(categoryRepository.findByName(req.getCategory().getName()))
+                .orElseGet(() -> {
+                    Category newCategory = new Category(req.getCategory().getName());
+                    return categoryRepository.save(newCategory);
+                });
+        req.setCategory(category);
+        return productRepository.save(createProducts(req, category));
+
     }
+
+    private Product createProducts(ProductRequest req, Category category){
+        return new Product(
+                req.getName(),
+                req.getBrand(),
+                req.getPrice(),
+                req.getInventory(),
+                req.getDescription(),
+                category
+        );
+    }
+
 
     @Override
     public Product getProductById(Long id) {
@@ -36,8 +63,23 @@ public class ProductService implements IProductService {
     }
 
     @Override
-    public void updateProduct(Product product, Long id) {
+    public Product updateProduct(ProductUpdateRequest req, Long id) {
+        return productRepository.findById(id)
+                .map(existingProduct -> updateExistingProduct(existingProduct, req))
+                .map(productRepository::save)
+                .orElseThrow(() -> new ProductNotFoundException("Product not found"));
 
+    }
+
+    private Product updateExistingProduct(Product product, ProductUpdateRequest req){
+        product.setName(req.getName());
+        product.setBrand(req.getBrand());
+        product.setPrice(req.getPrice());
+        product.setInventory(req.getInventory());
+        product.setDescription(req.getDescription());
+        Category category = categoryRepository.findByName(req.getCategory().getName());
+        product.setCategory(category);
+        return product;
     }
 
     @Override
