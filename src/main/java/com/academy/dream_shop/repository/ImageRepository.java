d725d0f23bd1ccd3c3d0daad94c75a910837683f
Svetoslavss <svetoslavd.stoyanov@gmail.com>
2025-01-25 23:40:56 +0200

    ImageRepository created

---
+++ src/main/java/com/academy/dream_shop/repository/ImageRepository.java
@@ -0,0 +1,8 @@
+package com.academy.dream_shop.repository;
+
+import com.academy.dream_shop.models.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+
+}
