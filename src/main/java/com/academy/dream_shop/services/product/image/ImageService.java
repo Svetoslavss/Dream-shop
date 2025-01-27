f9e5f8217401d4e2f7710ff1bb0a9b2fba80e991
Svetoslavss <svetoslavd.stoyanov@gmail.com>
2025-01-27 20:39:53 +0200

    Create ImageService to implement methods from Image interface

--- src/main/java/com/academy/dream_shop/services/product/image/ImageService.java
+++ src/main/java/com/academy/dream_shop/services/product/image/ImageService.java
@@ -1,20 +1,22 @@
 package com.academy.dream_shop.services.product.image;
 
-import com.academy.dream_shop.exceptions.ProductNotFoundException;
 import com.academy.dream_shop.exceptions.ResourceNotFound;
 import com.academy.dream_shop.models.Image;
 import com.academy.dream_shop.models.Product;
+import com.academy.dream_shop.models.dto.ImageDto;
 import com.academy.dream_shop.repository.ImageRepository;
-import com.academy.dream_shop.repository.ProductRepository;
 import com.academy.dream_shop.services.product.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.sql.rowset.serial.SerialBlob;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
 
+@Service
 public class ImageService implements IImageService {
 
 
@@ -22,7 +24,7 @@ public class ImageService implements IImageService {
     private ImageRepository imageRepository;
 
     @Autowired
-    private ProductRepository productRepository;
+    private ProductService productService;
 
 
     @Override
@@ -32,7 +34,7 @@ public class ImageService implements IImageService {
 
     @Override
     public void deleteImageById(Long id) {
-        if (imageRepository.existsById(id)){
+        if (imageRepository.existsById(id)) {
             imageRepository.deleteById(id);
         } else {
             throw new ResourceNotFound("Image not found");
@@ -40,36 +42,56 @@ public class ImageService implements IImageService {
     }
 
     @Override
-    public Image saveImage(MultipartFile file, Long productId) {
-       try {
-           Product product = productRepository.findById(productId)
-                   .orElseThrow(() -> new ProductNotFoundException("Product not found"));
+    public List<ImageDto> saveImage(List<MultipartFile> files, Long productId) {
+        Product product = productService.getProductById(productId);
+        List<ImageDto> imgDto = new ArrayList<>();
+        for (MultipartFile file : files) {
+            try {
+                Image image = new Image();
+                image.setFileName(file.getOriginalFilename());
+                image.setFileType(file.getContentType());
+                image.setImage(new SerialBlob(file.getBytes()));
+                image.setProduct(product);
 
-           Image image = new Image();
-           image.setFileName(file.getOriginalFilename());
-           image.setFileType(file.getContentType());
-           image.setImage(new SerialBlob(file.getBytes()));
-           image.setImageUrl("/image/" + file.getOriginalFilename());
-           image.setProduct(product);
+                String imageUrl = "/api/v1/images/image/download/" + image.getId();
+                image.setImageUrl(imageUrl);
 
-           return imageRepository.save(image);
-       } catch (IOException | SQLException e){
-           throw new RuntimeException("Error processing the image file. ");
-       }
+                Image saveImage = imageRepository.save(image);
+                ImageDto imageDto = convertImageToDto(saveImage);
+                imgDto.add(imageDto);
 
+            } catch (IOException | SQLException e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+        return imgDto;
     }
 
-    @Override
-    public void updateImage(MultipartFile file, Long imageId) {
-        Image image = new Image();
-        try {
-            image.setFileName(file.getOriginalFilename());
-            image.setFileType(file.getContentType());
-            image.setImage(new SerialBlob(file.getBytes()));
-            imageRepository.save(image);
-        } catch (IOException | SQLException e){
-            throw new RuntimeException(e);
+    private ImageDto convertImageToDto(Image image){
+        return new ImageDto(image.getId(), image.getFileName(), image.getFileType(), image.getImageUrl());
+    }
+
+        @Override
+        public ImageDto updateImage (MultipartFile file, Long imageId){
+            Image image = imageRepository.findById(imageId)
+                    .orElseThrow(() -> new ResourceNotFound("Image does not found"));
+            try {
+                image.setFileName(file.getOriginalFilename());
+                image.setFileType(file.getContentType());
+                image.setImage(new SerialBlob(file.getBytes()));
+                Image savedImage = imageRepository.save(image);
+                return updateDtoImage(savedImage);
+            } catch (IOException | SQLException e) {
+                throw new RuntimeException(e);
+            }
         }
 
+        private ImageDto updateDtoImage(Image image) {
+        return new ImageDto(
+                image.getId(),
+                image.getFileName(),
+                image.getFileType(),
+                image.getImageUrl()
+        );
+        }
     }
-}
