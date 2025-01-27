8e6c3df60571251b2573a1d6469a6dc4558ee753
Svetoslavss <svetoslavd.stoyanov@gmail.com>
2025-01-28 00:06:20 +0200

    AuthenticationManager bean implemented

--- src/main/java/com/academy/dream_shop/config/config.java
+++ src/main/java/com/academy/dream_shop/config/config.java
@@ -3,10 +3,13 @@ package com.academy.dream_shop.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -29,5 +32,10 @@ public class config {
 
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 }
