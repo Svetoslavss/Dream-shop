6f2d00f6c6fc740be9a1fcbb3b1b8358a3760134
Svetoslavss <svetoslavd.stoyanov@gmail.com>
2025-01-27 20:38:36 +0200

    Image inteface for abstaction created

--- src/main/java/com/academy/dream_shop/services/product/image/IImageService.java
+++ src/main/java/com/academy/dream_shop/services/product/image/IImageService.java
@@ -1,12 +1,15 @@
 package com.academy.dream_shop.services.product.image;
 
 import com.academy.dream_shop.models.Image;
+import com.academy.dream_shop.models.dto.ImageDto;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface IImageService {
 
     Image getImageById(Long id);
     void deleteImageById(Long id);
-    Image saveImage(MultipartFile file, Long productId);
-    void updateImage(MultipartFile file, Long imageId);
+    List<ImageDto> saveImage(List<MultipartFile> file, Long productId);
+    ImageDto updateImage(MultipartFile file, Long imageId);
 }
